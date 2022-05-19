bb62d1cd8c5102e5f24ab6506986c2690326eb38
Austin Johnson <austintjohnson92@gmail.com>
2022-05-18 21:40:03 -0700

    added a little more styling

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ function App(props) {
      props.getActivity()
  }, [])
 
+ 
+ 
+
 return (
     <div className="App">
        
@@ -20,12 +23,12 @@ return (
       <h2>CLICK THE BUTTON BELOW FOR SOME COOL THINGS TO DO!</h2>
       </div>
       <button id="btn" onClick={() => props.getActivity()}>CLICK ME!</button>
-<div className="response-div">
+<div className="response-div" id="hidden">
     <h3>{props.activity}</h3>
     </div>
      
      <footer>
-     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,288L48,272C96,256,192,224,288,224C384,224,480,256,576,256C672,256,768,224,864,229.3C960,235,1056,277,1152,293.3C1248,309,1344,299,1392,293.3L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fillOpacity="1" d="M0,288L48,272C96,256,192,224,288,224C384,224,480,256,576,256C672,256,768,224,864,229.3C960,235,1056,277,1152,293.3C1248,309,1344,299,1392,293.3L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
      </footer>
 </div>
    
